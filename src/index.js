488dedea0dcb0d3dc2a53da959ace29bb0863ee4
mossa <martin.ossa.8@gmail.com>
2023-03-08 15:32:26 +0100

    Fix styling issues

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import './styles.css';
 
 const recentScores = document.getElementById('recent-scores');
 
-
 const singleScore = document.createElement('li');
 singleScore.innerHTML = (`<li>name: 100</li><li>name: 100</li>
-`)
+`);
 recentScores.appendChild(singleScore);
