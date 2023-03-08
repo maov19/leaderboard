c9eb3382be45a5e11c0579d31f69b4e4ed496af6
mossa <martin.ossa.8@gmail.com>
2023-03-08 15:26:29 +0100

    Add scores dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+
+const recentScores = document.getElementById('recent-scores');
+
+
+const singleScore = document.createElement('li');
+singleScore.innerHTML = (`<li>name: 100</li><li>name: 100</li>
+`)
+recentScores.appendChild(singleScore);
